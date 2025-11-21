4801001f4bc06bda1000e64ababb82e2660af73f
Divine Patrick <patrickdivine70@gmail.com>
2025-11-21 23:15:41 +0100

    updated dashboard, blog, image generator page, community page, rm object page, createItem page, write article, review resume and rm bg pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import GenerateImages from './pages/GenerateImages'
 import RemoveBackground from './pages/RemoveBackground'
 import RemoveObject from './pages/RemoveObject'
 import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           <Route path='remove-background' element={<RemoveBackground />} />
           <Route path='remove-object' element={<RemoveObject />} />
           <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
         </Route>
       </Routes>
     </div>
